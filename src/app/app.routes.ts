f2f5fb81d6deab1a286fd369f1d9f4e6fb649edd
lsvelikov <lubomirvelikov@yahoo.com>
2024-12-10 09:38:42 +0100

    routes changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { UserTablesComponent } from './user-tables/user-tables.component';
+import { UserTablesComponent } from './login/user-tables/user-tables.component';
 import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
